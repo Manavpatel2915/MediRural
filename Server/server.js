3fea4ffaff4cd67eb9a9acd8ab82ced856553edd
vaibhav2628 <vaibhavpatil2579@gmail.com>
2025-06-28 15:04:09 +0530

    fixed cors error

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://medirural.vercel.app', 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
